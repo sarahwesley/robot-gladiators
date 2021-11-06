3aab7b81bc9f7808692f77f491d904b141483ec0
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-06 15:54:01 -0400

    player can create name for robot and it will store in console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
 window.alert("This is an alert!! JavaScript is running!")
+// this creates a function named "fight"
+
+function fight () {
+    window.alert("The Fight has begun!");
+}
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+// note the lack of quotation marks aroung playerName
+console.log(playerName);
+console.log("this logs a string, good for leaving yhourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("our robot's name is " + playerName);
+
+var name = "Sarah";
+console.log(name);
+
+// fight();
