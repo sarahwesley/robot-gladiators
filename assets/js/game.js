966cd6a655e1097b083f642ac985da59987cd0c8
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-07 07:50:54 -0500

    shop functoin

--- assets/js/game.js
+++ assets/js/game.js
@@ -83,11 +83,19 @@ for(var i = 0; i < enemyNames.length; i++) {
     // debugger;
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
- } else {
+    fight(pickedEnemyName); 
+    {
+    if (playerHealth > 0 && i < enemyNames.length - 1) {
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round!");
+        
+        if (storeConfirm){
+        shop();
+        }
+    }
+    else {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
-      }
+    }
   
 }
 endGame();
@@ -111,7 +119,52 @@ else {
   window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
 };
-    
+
+var shop = function() {
+    // ask player what they'd like to do
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+  // use switch to carry out action
+  switch (shopOptionPrompt) {
+    case "REFILL": // new case
+    case "refill":
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+  
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+  
+      break;
+    case "UPGRADE": // new case
+    case "upgrade":
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+  
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+  
+      break;
+    case "LEAVE": // new case
+    case "leave":
+      window.alert("Leaving the store.");
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
+  }
+};
+
+startGame();   
 
 // game states
 // "WIN" - player robot has defeated all enemy-robots
